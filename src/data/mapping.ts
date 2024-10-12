035db842cd094c7905e07d540404fd9245652e5d
Stéphane Bisinger <stephane@sbisinger.ch>
2024-10-13 00:48:24 +0200

    Add latest spec-closed games

--- src/data/mapping.ts
+++ src/data/mapping.ts
@@ -1,4 +1,5 @@
 export const mapDraftNameToGameNameMapping = {
+    "Andalucia": "SPEC Andalucia",
     "Amazon Tunnel": 'SPEC Amazon Tunnel',
     "Dzong": 'SPEC Dzong',
     "Fortified Clearing": 'SPEC Fortified Clearing',
@@ -6,11 +7,13 @@ export const mapDraftNameToGameNameMapping = {
     "Glade": 'SPEC Glade',
     "Golden Fort": 'SPEC Golden-Fort',
     "Hidden Forts": 'SPEC Hidden Forts',
+    "Palisade Arena": 'SPEC Palisade Arena',
     "Ring of Reeds": 'SPEC Ring of Reeds',
     "ZeWall": 'SPEC ZeWall',
 };
 
 export const GameNameMappingToDisplayName = {
+    'SPEC Andalucia': "Andalucia",
     'SPEC Amazon Tunnel': "Amazon Tunnel",
     'SPEC Dzong': "Dzong",
     'SPEC Fortified Clearing': "Fortified Clearing",
@@ -18,6 +21,7 @@ export const GameNameMappingToDisplayName = {
     'SPEC Glade': "Glade",
     'SPEC Golden-Fort': "Golden Fort",
     'SPEC Hidden Forts': "Hidden Forts",
+    'SPEC Palisade Arena': "Palisade Arena",
     'SPEC Ring of Reeds': "Ring of Reeds",
     'SPEC ZeWall': "ZeWall",
 };
